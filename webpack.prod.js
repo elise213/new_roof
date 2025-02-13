53ee484fdbc502978026036a36f3c4d2597812f3
Mara <maracodes213@gmail.com>
2025-02-13 11:31:56 -0800

    saving

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,18 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const Dotenv = require('dotenv-webpack');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const Dotenv = require("dotenv-webpack");
+
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: '/'
-    },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+  mode: "production",
+  output: {
+    publicPath: "/",
+  },
+  plugins: [
+    new Dotenv({
+      safe: true,
+      systemvars: true,
+      silent: true, // Suppresses errors for missing env variables
+      defaults: true, // Uses .env.example if available
+    }),
+  ],
 });
